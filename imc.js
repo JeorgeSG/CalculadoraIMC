10738632d9a8f64265c3afce61464369b5ca2ff8
JeorgeSG <145723007+JeorgeSG@users.noreply.github.com>
2026-03-30 01:47:58 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,26 @@
+const calcular = document.getElementById("calcular");
+
+function imc() {
+  const nome = document.getElementById("nome").value;
+  const altura = document.getElementById("altura").value;
+  const peso = document.getElementById("peso").value;
+  const resultado = document.getElementById("resultado");
+
+  if (nome !== "" && altura !== "" && peso !== "") {
+    const ValorIMC = (peso / (altura * altura)).toFixed(1);
+    let classificacao = "";
+
+    if (ValorIMC < 18.5) {
+      classificacao = "Abaixo do peso";
+    }else if (ValorIMC < 25){
+      classificacao = "Peso ideal"
+    }
+ 
+
+    resultado.textContent = `${nome} seu IMC é ${ValorIMC} e você está ${classificacao}`;
+  } else {
+    resultado.textContent = "Preencha todos os campos!";
+  }
+}
+
+calcular.addEventListener("click", imc);
